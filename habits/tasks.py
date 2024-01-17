2b9705f41ea1dd94d107d6213dca03157bab47e6
alex <sanchez005909@gmail.com>
2024-01-17 21:19:40 +0300

    change

--- habits/tasks.py
+++ habits/tasks.py
@@ -3,11 +3,12 @@ from datetime import datetime, timedelta
 import requests
 from celery import shared_task
 import telebot
+from django.utils import timezone
 
 from habits.models import Habit
-from users.models import User
 
 bot = telebot.TeleBot(os.getenv('TELEGRAM_BOT_TOKEN'))
+
 
 @shared_task
 def send_habit():
@@ -16,27 +17,27 @@ def send_habit():
                 f'{os.getenv("TELEGRAM_BOT_TOKEN")}/sendMessage')
     dt_now = datetime.now().strftime('%y-%m-%d %H:%M')
 
-    users = User.objects.all()
-    for user in users:
-        habits = Habit.objects.filter(owner=user)
-        for habit in habits:
-            time_do_it = habit.time_do_it.strftime('%y-%m-%d %H:%M')
-            if dt_now == time_do_it:
-                if habit.is_nice_habit:
-                    text = (f'Я буду {habit.action} в {habit.time_do_it} '
-                            f'в {habit.place}')
-                elif habit.related_habit:
-                    text = (
-                        f'Я буду {habit.action} в {habit.time_do_it} '
-                        f'в {habit.place}. За эту полезную привычку, '
-                        f'я сделаю приятную: {habit.related_habit}')
-                elif habit.prize:
-                    text = (f'Я буду {habit.action} в {habit.time_do_it} '
-                            f'в {habit.place}. Вознаграждение: {habit.prize}')
+    habits = Habit.objects.all()
+    for habit in habits:
+        time_do_it = (timezone.localtime(habit.time_do_it)
+                      .strftime('%y-%m-%d %H:%M'))
+        if dt_now == time_do_it:
+            time = (timezone.localtime(habit.time_do_it).time()
+                    .strftime('%H:%M'))
+            if habit.is_nice_habit:
+                text = (f'Я буду {habit.action} в {time} '
+                        f'в {habit.place}')
+            elif habit.related_habit:
+                text = (
+                    f'Я буду {habit.action} в {time} '
+                    f'в {habit.place}. За эту полезную привычку, '
+                    f'я сделаю приятную: {habit.related_habit}')
+            elif habit.prize:
+                text = (f'Я буду {habit.action} в {time} '
+                        f'в {habit.place}. Вознаграждение: {habit.prize}')
 
-                params = {"chat_id": user.chat_id, "text": text}
+            params = {"chat_id": habit.owner.chat_id, "text": text}
 
-                requests.post(url=url_send, params=params)
-                habit.date += timedelta(days=habit.period)
-                habit.save()
-
+            requests.post(url=url_send, params=params)
+            habit.time_do_it += timedelta(days=habit.period)
+            habit.save()
